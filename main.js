67cde55fea09ca5c625e946f748493d5eb1dd96a
Jacob Doren <Jacob Doren>
2022-05-15 21:35:16 -0400

    added basic movement

--- main.js
+++ main.js
@@ -7,41 +7,61 @@ canvas.height = innerHeight
 const gravity = 0.5
 
 class Player {
-    constructor(gravity) {
-        this.position = {
-            x: 100,
-            y: 100
-        }
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
-        this.width = 100
-        this.height = 100
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100
     }
+    this.velocity = {
+      x: 0,
+      y: 0
+    }
+    this.width = 100
+    this.height = 100
+  }
 
-    draw() {
-        ctx.fillStyle = 'red'
-        ctx.fillRect(this.position.x, this.position.y,this.width, this.height)
-    }
+  draw() {
+    ctx.fillStyle = 'red'
+    ctx.fillRect(this.position.x, this.position.y,this.width, this.height)
+  }
 
-    update() {
-        this.position.y += this.velocity.y
-        console.log(canvas.height)
-        if(this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } else this.velocity.y += gravity
-        this.draw()
-    }
+  update() {
+    this.position.y += this.velocity.y
+    this.position.x += this.velocity.x
+    if(this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0
+    } else this.velocity.y += gravity
+    this.draw()
+  }
 }
 
-const player = new Player()
+const player = new Player(gravity)
 
 // Loop and update player
 function animate() {
-    requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
+  requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  player.update()
 }
 
 animate()
+
+addEventListener('keydown', e => {
+  switch(e.key) {
+    case 'w': 
+      console.log('up')
+      player.velocity.y -= 8
+      break
+    case 'a':
+      console.log('left')
+      player.velocity.x -= 8
+      break
+    case 's': 
+      console.log('down')
+      break
+    case 'd':
+      console.log('right')
+      player.velocity.x += 8
+      break
+  }
+})
